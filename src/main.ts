a0ab4876ebe0b6162e60202f8418700d2bae4699
Edwin Cosme <adriancosme15@gmail.com>
2024-07-01 17:00:55 -0500

    chore: add data validation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { useContainer } from 'class-validator';
+import { ValidationPipe } from '@nestjs/common';
+import validationOptions from './utils/validation-options';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+  app.useGlobalPipes(new ValidationPipe(validationOptions));
   await app.listen(3000);
 }
 bootstrap();
