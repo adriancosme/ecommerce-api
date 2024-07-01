21b9af83856b7c048ab9fe5ce6f8affba0a7a990
Edwin Cosme <adriancosme15@gmail.com>
2024-07-01 17:50:42 -0500

    chore: add app configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import appConfig from './config/app.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [],
+      load: [appConfig],
       envFilePath: ['.env'],
     }),
   ],
